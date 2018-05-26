898d6ba1f9386495fc377af3b3ee449ce42dbf94
Victor <victorpereiradepaula@gmail.com>
2018-05-26 16:06:50 -0300

    image on splash screen

--- app/src/main/java/com/example/dell/petsforall/SplashScreen.java
+++ app/src/main/java/com/example/dell/petsforall/SplashScreen.java
@@ -3,11 +3,14 @@ package com.example.dell.petsforall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
+    private static final int SPLASH_SCREEN_TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
+
+
     }
 }
