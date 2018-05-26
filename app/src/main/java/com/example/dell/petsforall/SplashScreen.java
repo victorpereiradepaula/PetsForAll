dbd925a0723bd637ab42479a82d0606ec1f506ab
Victor <victorpereiradepaula@gmail.com>
2018-05-26 16:21:55 -0300

    splash screen working

--- app/src/main/java/com/example/dell/petsforall/SplashScreen.java
+++ app/src/main/java/com/example/dell/petsforall/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.example.dell.petsforall;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +13,13 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME);
     }
 }
